4e6e84d108cceb1388d0de84feafe8094b7470a8
cchitla <chrischitla@gmail.com>
2020-04-10 15:56:14 -0700

    added category to article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  access all: [:index, :show], admin: :all
+  access all: [:index, :show, :new, :edit, :create, :update, :destroy], admin: :all
 
   # GET /articles
   def index
@@ -54,6 +54,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def article_params
-      params.require(:article).permit(:title, :content, :user_id)
+      params.require(:article).permit(:title, :content, :user_id, :category_id)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user 
   belongs_to :category
+
+  validates_presence_of :category_id
 end
